46e40e4d2ef2b7f720856ff86612eb209bb5d400
Alexey Zabelin <zabelin.alex@gmail.com>
2015-12-15 15:52:20 -0500

    Edit the prompt config

--- tag-zsh/config/zsh/configs/prompt.zsh
+++ tag-zsh/config/zsh/configs/prompt.zsh
@@ -1,6 +1,6 @@
 # modify the prompt to contain git branch name if applicable
 git_prompt_info() {
-  current_branch=$(git current-branch 2> /dev/null)
+  current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
   if [[ -n $current_branch ]]; then
     echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
   fi
