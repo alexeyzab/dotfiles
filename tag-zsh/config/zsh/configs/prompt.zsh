0070b0c973cadc748afc91624ac4a5797067a61a
Alexey Zabelin <zabelin.alex@gmail.com>
2016-03-29 09:15:20 -0400

    Change prompt colors to bold

--- tag-zsh/config/zsh/configs/prompt.zsh
+++ tag-zsh/config/zsh/configs/prompt.zsh
@@ -2,8 +2,8 @@
 git_prompt_info() {
   current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
   if [[ -n $current_branch ]]; then
-    echo " %{$fg[green]%}$current_branch%{$reset_color%}"
+    echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
   fi
 }
 setopt promptsubst
-PS1='${SSH_CONNECTION+"%{$fg[green]%}%n@%m:"}%{$fg[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
