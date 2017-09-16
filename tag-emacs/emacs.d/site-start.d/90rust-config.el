83a60f9a8993e11fdd017e01a1a3c69d4cd613a2
Alexey Zabelin <hello@alexeyzabelin.com>
2017-09-15 23:41:46 -0400

    Update Rust config

--- tag-emacs/emacs.d/site-start.d/90rust-config.el
+++ tag-emacs/emacs.d/site-start.d/90rust-config.el
@@ -5,8 +5,35 @@
 (use-package toml-mode)
 
 ;; racer
+(use-package racer)
+(use-package company-racer)
+(with-eval-after-load 'company
+      (add-to-list 'company-backends 'company-racer))
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
+
+;; cargo
+(use-package cargo
+  :bind
+  ("C-c C-c C-b" . cargo-process-build)
+  ("C-c C-c C-r" . cargo-process-run)
+  ("C-c C-c C-t" . cargo-process-test)
+  ("C-c C-c C-e" . cargo-process-bench)
+  ("C-c C-c C-l" . cargo-process-clean)
+  ("C-c C-c C-d" . cargo-process-doc)
+  ("C-c C-c C-v" . cargo-process-doc-open)
+  ("C-c C-c C-n" . cargo-process-new)
+  ("C-c C-c C-i" . cargo-process-init)
+  ("C-c C-c C-x" . cargo-process-run-example)
+  ("C-c C-c C-s" . cargo-process-search)
+  ("C-c C-c C-u" . cargo-process-update)
+  ("C-c C-c C-c" . cargo-process-repeat)
+  ("C-c C-c C-f" . cargo-process-current-test)
+  ("C-c C-c C-o" . cargo-process-current-file-tests)
+  ("C-c C-c C-m" . cargo-process-fmt)
+  ("C-c C-c C-k" . cargo-process-check)
+  ("C-c C-c C-S-k" . cargo-process-clippy)
+)
 
 ;; completion
 (add-hook 'rust-mode-hook #'racer-mode)
@@ -21,10 +48,12 @@
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
 
 ;; rustfmt
+(use-package rustfmt)
 (add-hook 'rust-mode-hook (lambda () (local-set-key (kbd "C-c <tab>")
 						    #'rust-format-buffer)))
 
 ;; flycheck
+(use-package flycheck-rust)
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
 ;; rainbow-delimeters for haskell-mode
