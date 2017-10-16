c03307f5720b0cd1f5d29f1bc535ae2357504483
Alexey Zabelin <hello@alexeyzabelin.com>
2017-10-15 22:45:47 -0400

    Add NixOS configs

---
+++ tag-nixos/config/nixpkgs/home.nix
@@ -0,0 +1,111 @@
+{ pkgs, ... }:
+
+let
+  xmonad = pkgs.xmonad-with-packages.override {
+    packages = self: [
+      self.xmonad-contrib
+      self.xmonad-extras
+    ];
+  };
+in
+{
+  home.packages = with pkgs; [
+    # rescuetime
+    arandr
+    arc-theme
+    aspell
+    bleachbit
+    cabal-install
+    cabal2nix
+    ctags
+    deluge
+    dmenu
+    docker
+    dropbox
+    emacs
+    evince
+    feh
+    fzf
+    ghc
+    gnome3.gnome-tweak-tool
+    gnome3.nautilus
+    gnumake
+    gnupg
+    haskellPackages.codex
+    haskellPackages.hasktags
+    haskellPackages.hlint
+    haskellPackages.hpack
+    haskellPackages.threadscope
+    haskellPackages.xmobar
+    haskellPackages.xmonad
+    htop
+    lxappearance
+    mirage
+    mpv
+    ncdu
+    nix-prefetch-git
+    nix-repl
+    nix-zsh-completions
+    nodePackages.gulp
+    nodePackages.npm
+    nodejs
+    pinentry
+    postgresql
+    python27Packages.udiskie
+    rcm
+    rcm
+    ripgrep
+    rofi
+    rustfmt
+    rustup
+    rxvt_unicode
+    sass
+    skype
+    slock
+    spotify
+    stack
+    stalonetray
+    texlive.combined.scheme-full
+    tmux
+    tree
+    unzip
+    urlview
+    urxvt_autocomplete_all_the_things
+    urxvt_perls
+    vim
+    zathura
+    networkmanagerapplet
+  ];
+
+  programs.home-manager.enable = true;
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+
+  services.network-manager-applet.enable = true;
+
+  xsession = {
+    enable = true;
+    windowManager.command = "${xmonad}/bin/xmonad";
+    initExtra = ''
+      xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --off
+      xinput --set-button-map "Logitech USB Trackball" 1 8 3 4 5 6 7 2 2
+      xinput --set-prop "Logitech USB Trackball" "Evdev Wheel Emulation" 1
+      xinput --set-prop "Logitech USB Trackball" "Evdev Wheel Emulation Button" 8
+      xinput --set-prop "Logitech USB Trackball" "Evdev Wheel Emulation Axes" 6 7 4 5
+      xinput --set-prop "Logitech USB Trackball" "Evdev Middle Button Emulation" 1
+      xinput --set-prop "Logitech USB Trackball" "Evdev Middle Button Emulation Button" 9
+      xrdb ~/.Xresources
+      xset s off -dpms
+      xsetroot -cursor_name left_ptr;
+      stalonetray &
+      rescuetime &
+      dropbox &
+      nm-applet &
+      feh --bg-scale ~/.local/share/city_wallpaper.jpg
+    '';
+  };
+}
