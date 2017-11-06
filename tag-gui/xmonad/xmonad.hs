6b55755888a6bfb518cfc349e67a982383b2d996
Alexey Zabelin <hello@alexeyzabelin.com>
2017-11-06 16:59:14 -0500

    Switch from xmobar to taffybar

--- tag-gui/xmonad/xmonad.hs
+++ tag-gui/xmonad/xmonad.hs
@@ -7,14 +7,46 @@ import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.EwmhDesktops
 import           XMonad.Hooks.ManageDocks
 import           XMonad.Hooks.ManageHelpers
+import           XMonad.Hooks.UrgencyHook
 import           XMonad.Layout.CenteredMaster
 import           XMonad.Layout.Maximize
+import           XMonad.Layout.NoBorders
 import qualified XMonad.StackSet              as W
 import           XMonad.Util.EZConfig
 import           XMonad.Util.NamedScratchpad
 import           XMonad.Util.Run
 
-myWorkspaces = ["1","2","3","4","5","6","7","8"]
+
+main :: IO ()
+main = do
+    xmonad =<< statusBar "~/.local/bin/my-taffybar" defaultPP toggleStrutsKey (ewmh $ docks $ myConfig)
+
+myConfig = def
+      { manageHook = myManageHook <+> manageHook def <+> manageDocks
+                                  <+> namedScratchpadManageHook scratchpads
+      , handleEventHook = docksEventHook <+> handleEventHook def
+      , layoutHook = avoidStrutsOn [U] $ smartBorders $ layoutHook def
+      , modMask = mod4Mask
+      , terminal = "urxvtc"
+      , workspaces = myWorkspaces
+      , normalBorderColor = "#000000"
+      , focusedBorderColor = "#839496"
+      } `additionalKeysP`
+      [ ("M-p", spawn "rofi -show run -m -4 -font 'fantasque sans mono 12'")
+      , ("M-t", spawn "rofi -show window -m -4 -font 'fantasque sans mono 12'")
+      , ("M-S-l", spawn "~/.local/bin/better-slock")
+      , ("M-s", namedScratchpadAction scratchpads "scratch")
+      , ("M-u", namedScratchpadAction scratchpads "mail")
+      , ("M-z", namedScratchpadAction scratchpads "zeal")
+      , ("M-f", spawn "amixer -q set Master 5%+")
+      , ("M-d", spawn "amixer -q set Master 5%-")
+      , ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 5%+")
+      , ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 5%-")
+      , ("<XF86AudioMute>", spawn "amixer -q set Master 1+ toggle")
+      , ("M-y", withFocused (sendMessage . maximizeRestore))
+      ]
+
+myWorkspaces = ["1","2","3","4","5","6","7","8", "9"]
 myManageHook =
   composeAll
     [ className =? "chromium-browser" --> doShift "2"
@@ -25,55 +57,14 @@ myManageHook =
   where
     role = stringProperty "WM_WINDOW_ROLE"
 
-main :: IO ()
-main = do
-    xmproc <- spawnPipe "/home/alexeyzab/.nix-profile/bin/xmobar /home/alexeyzab/.config/xmobar/xmobarrc"
-
-    xmonad $ ewmh def
-      { manageHook = myManageHook <+> manageDocks
-                                  <+> manageHook def
-                                  <+> namedScratchpadManageHook scratchpads
-      , handleEventHook = docksEventHook <+> handleEventHook def
-      , layoutHook = maximize $ avoidStruts $ layoutHook def
-      , logHook = updatePointer (0.5, 0.5) (0, 0) >>
-        dynamicLogWithPP xmobarPP
-          { ppOutput = hPutStrLn xmproc
-          , ppCurrent = xmobarColor "#839496" ""
-          , ppTitle = xmobarColor "#bfbfbf" "" . shorten 50
-          , ppLayout = const ""
-          }
-      , modMask = mod4Mask
-      , terminal = "urxvtc"
-      , workspaces = myWorkspaces
-      , normalBorderColor = "#000000"
-      , focusedBorderColor = "#839496"
-      } `additionalKeysP`
-      [ ("M-p", spawn "rofi -show run -m -4 -font 'fantasque sans mono 12'")
-      , ("M-S-l", spawn "~/.local/bin/better-slock")
-      , ("M-b", spawn "~/.local/bin/toggle_stalonetray" >> sendMessage ToggleStruts)
-      , ("M-s", namedScratchpadAction scratchpads "scratch")
-      , ("M-u", namedScratchpadAction scratchpads "mail")
-      , ("M-z", namedScratchpadAction scratchpads "zeal")
-      , ("M-f", spawn "amixer -q set Master 5%+")
-      , ("M-d", spawn "amixer -q set Master 5%-")
-      , ("M-c", spawn "amixer -q set Master 1+ toggle")
-      , ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 5%+")
-      , ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 5%-")
-      , ("<XF86AudioMute>", spawn "amixer -q set Master 1+ toggle")
-      -- , ("M-f", spawn "pactl set-sink-volume 1 +5%")
-      -- , ("M-d", spawn "pactl set-sink-volume 1 -5%")
-      -- , ("M-c", spawn "pactl set-sink-mute 1 toggle")
-      -- , ("M-i", spawn "light -s mba6x_backlight -U 10")
-      -- , ("M-o", spawn "light -s mba6x_backlight -A 10")
-      , ("M-y", withFocused (sendMessage . maximizeRestore))
-      ]
+toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 scratchpads :: [NamedScratchpad]
 scratchpads =
     [ NS "mail" "urxvtc -e mutt" (title =? "mutt")
         (customFloating $ W.RationalRect 0.05 0.1 0.9 0.8)
     , NS "scratch" "urxvtc --title scratch" (title =? "scratch")
-        -- (customFloating $ W.RationalRect 0.6 0.8 0.35 0.1)
         (customFloating $ W.RationalRect 0.3 0.5 0.4 0.2)
     , NS "zeal" "zeal" (className =? "Zeal")
         (customFloating $ W.RationalRect 0.05 0.1 0.9 0.8)
