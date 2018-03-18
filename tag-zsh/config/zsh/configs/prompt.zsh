41d132e6a0e2ee215699203453b3c01cfef7f864
Alexey Zabelin <hello@alexeyzabelin.com>
2018-03-18 17:25:06 -0400

    Zsh update

--- tag-zsh/config/zsh/configs/prompt.zsh
+++ tag-zsh/config/zsh/configs/prompt.zsh
@@ -1,18 +1,58 @@
 # modify the prompt to contain git branch name if applicable
 local ret_status="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
 
-PROMPT='${ret_status}%{$fg_bold[green]%}%p %{$fg_bold[blue]%}%c $(git_prompt_info)% %{$reset_color%} λ: '
-
-ZSH_THEME_GIT_PROMPT_CLEAN=") %{$fg_bold[green]%}✔ "
-ZSH_THEME_GIT_PROMPT_DIRTY=") %{$fg_bold[yellow]%}✗ "
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[cyan]%}("
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-
 git_prompt_info() {
   current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
   if [[ -n $current_branch ]]; then
     echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
   fi
 }
+
+CURRENT_BG='NONE'
+
+() {
+  local LC_ALL="" LC_CTYPE="en_US.UTF-8"
+  SEGMENT_SEPARATOR=$'\ue0b0'
+}
+
+prompt_segment() {
+  local bg fg
+  [[ -n $1 ]] && bg="%K{$1}" || bg="%k"
+  [[ -n $2 ]] && fg="%F{$2}" || fg="%f"
+  if [[ $CURRENT_BG != 'NONE' && $1 != $CURRENT_BG ]]; then
+    echo -n " %{$bg%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR%{$fg%} "
+  else
+    echo -n "%{$bg%}%{$fg%} "
+  fi
+  CURRENT_BG=$1
+  [[ -n $3 ]] && echo -n $3
+}
+
+prompt_nix_shell() {
+  if [[ -n "$IN_NIX_SHELL" ]]; then
+    if [[ -n $NIX_SHELL_PACKAGES ]]; then
+      local package_names=""
+      local packages=($NIX_SHELL_PACKAGES)
+      for package in $packages; do
+        package_names+=" ${package##*.}"
+      done
+      prompt_segment black yellow "{$package_names }"
+    elif [[ -n $name ]]; then
+      local cleanName=${name#interactive-}
+      cleanName=${cleanName%-environment}
+      prompt_segment black yellow "{ $cleanName }"
+    else # This case is only reached if the nix-shell plugin isn't installed or failed in some way
+      prompt_segment black yellow "nix-shell {}"
+    fi
+  fi
+}
+
+build_prompt() {
+  RETVAL=$?
+  prompt_nix_shell
+}
+
+PROMPT='$(build_prompt)${ret_status}%{$fg_bold[green]%}%p %{$fg_bold[blue]%}%c $(git_prompt_info)% %{$reset_color%} λ: '
+RPROMPT=''
+
 setopt promptsubst
-#PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) λ: '
