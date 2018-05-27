7677b0df3dc0d54f2cceeff29f91b7f51bf4b136
Alexey Zabelin <hello@alexeyzabelin.com>
2018-05-26 23:14:45 -0400

    Update configs on t480

--- tag-gui/config/taffybar/taffybar.hs
+++ tag-gui/config/taffybar/taffybar.hs
@@ -1,72 +1,202 @@
+-- module Main where
+
+-- import System.Taffybar.Context
+-- import System.Taffybar.Widget.Systray
+-- import System.Taffybar.Widget.TaffyPager
+-- import System.Taffybar.Widget.Pager
+-- import System.Taffybar.Widget.Battery
+-- import System.Taffybar.Widget.SimpleClock
+-- import System.Taffybar.Widget.NetMonitor
+
+-- import System.Taffybar.Widgets.PollingGraph
+
+-- import System.Taffybar.FreedesktopNotifications
+
+-- import Graphics.UI.Gtk
+-- import System.Information.Memory
+-- import System.Information.CPU
+
+-- textWidgetNew :: String -> IO Widget
+-- textWidgetNew str = do
+--     box <- hBoxNew False 0
+--     label <- labelNew $ Just str
+--     boxPackStart box label PackNatural 0
+--     widgetShowAll box
+--     return $ toWidget box
+
+-- memCallback = do
+--   mi <- parseMeminfo
+--   return [memoryUsedRatio mi]
+
+-- cpuCallback = do
+--   (userLoad, systemLoad, totalLoad) <- cpuLoad
+--   return [totalLoad, systemLoad]
+
+-- main = do
+--     let clock = textClockNew Nothing "<span fgcolor='#dfdfdf'>%-I:%M %p, %D</span>" 1
+--         pager = taffyPagerNew defaultPagerConfig
+--             { widgetSep = " :: "
+--             , activeWorkspace = colorize "#ffaa00" "" . escape
+--             , visibleWorkspace = colorize "#aa5500" "" . escape
+--             --, emptyWorkspace = colorize "#444444" "" . escape
+--             }
+--         -- note = notifyAreaNew defaultNotificationConfig
+--         tray = systrayNew
+--         sep = textWidgetNew " ::"
+--         memCfg = defaultGraphConfig { graphDataColors = [ (1, 0.1, 0.1, 1) ]
+--                                     , graphBackgroundColor = (0.1, 0.1, 0.1)
+--                                     , graphDirection = RIGHT_TO_LEFT
+--                                     , graphPadding = 3
+--                                     , graphWidth = 50
+--                                     , graphBorderWidth = 0
+--                                     , graphLabel = Just "mem"
+--                                     }
+--         cpuCfg = defaultGraphConfig {
+--             graphDataColors = [ (1, 0.1, 0.1, 1) ]
+--           , graphBackgroundColor = (0.1, 0.1, 0.1)
+--           , graphDirection = RIGHT_TO_LEFT
+--           , graphPadding = 3
+--           , graphWidth = 50
+--           , graphBorderWidth = 0
+--           , graphLabel = Just "cpu"
+--         }
+--         mem = pollingGraphNew memCfg 1 memCallback
+--         cpu = pollingGraphNew cpuCfg 1 cpuCallback
+--         -- battery = batteryBarNew defaultBatteryConfig 10
+--         -- batteryTime = textBatteryNew "| $time$" 10
+--         --net = netMonitorNewWith 1k.5 "wlp3s0" 2 (formatNetworkInfo defaultNetFormat)
+--         -- net = netMonitorNew 2 "wlp3s0"
+--     defaultTaffybar defaultTaffybarConfig
+--                         { startWidgets = [ pager ]
+--                         , endWidgets = [ mem, cpu, clock, tray ]
+--                         }
+
+-- module Main where
+
+-- import           System.Taffybar
+-- import           System.Taffybar.Information.CPU
+-- import           System.Taffybar.Information.Memory
+-- import           System.Taffybar.SimpleConfig
+-- import           System.Taffybar.Widget
+-- import           System.Taffybar.Widget.Workspaces
+-- import           System.Taffybar.Widget.Generic.PollingGraph
+
+-- memCallback = do
+--   mi <- parseMeminfo
+--   return [memoryUsedRatio mi]
+
+-- cpuCallback = do
+--   (userLoad, systemLoad, totalLoad) <- cpuLoad
+--   return [totalLoad, systemLoad]
+
+-- main = do
+--   let memCfg = defaultGraphConfig
+--                { graphDataColors = [(1, 0, 0, 1)]
+--                , graphLabel = Just "mem"
+--                }
+--       cpuCfg = defaultGraphConfig
+--                { graphDataColors =
+--                    [ (0, 1, 0, 1)
+--                    , (1, 0, 1, 0.5)
+--                    ]
+--                , graphLabel = Just "cpu"
+--                }
+--   let clock = textClockNew Nothing "<span fgcolor='#dfdfdf'>%-I:%M %p, %D</span>" 1
+--       workspaces = workspacesNew defaultWorkspacesConfig
+--       windows = windowsNew defaultWindowsConfig
+--       layout = layoutNew defaultLayoutConfig
+--       -- note = notifyAreaNew defaultNotificationConfig
+--       mpris = mpris2New
+--       mem = pollingGraphNew memCfg 1 memCallback
+--       cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+--       tray = sniTrayNew
+--   simpleTaffybar defaultSimpleTaffyConfig
+--                    { startWidgets = [ workspaces, layout, windows, note ]
+--                    , endWidgets = [ tray, clock, mem, cpu, mpris ]
+--                    }
+
+module Main where
+
 import System.Taffybar
+import System.Taffybar.Hooks
+import System.Taffybar.Information.CPU
+import System.Taffybar.Information.Memory
+import System.Taffybar.SimpleConfig
+import System.Taffybar.Widget
+import System.Taffybar.Widget.Generic.PollingGraph
+import System.Taffybar.Widget.Generic.PollingLabel
+import System.Taffybar.Widget.Util
+import System.Taffybar.Widget.Workspaces
 
+transparent = (0.0, 0.0, 0.0, 0.0)
+yellow1 = (0.9453125, 0.63671875, 0.2109375, 1.0)
+yellow2 = (0.9921875, 0.796875, 0.32421875, 1.0)
+green1 = (0, 1, 0, 1)
+green2 = (1, 0, 1, 0.5)
+taffyBlue = (0.129, 0.588, 0.953, 1)
 
-import System.Taffybar.Systray
-import System.Taffybar.TaffyPager
-import System.Taffybar.Pager
-import System.Taffybar.Battery
-import System.Taffybar.SimpleClock
-import System.Taffybar.NetMonitor
+myGraphConfig =
+  defaultGraphConfig
+  { graphPadding = 0
+  , graphBorderWidth = 0
+  , graphWidth = 50
+  , graphBackgroundColor = transparent
+  }
 
-import System.Taffybar.Widgets.PollingGraph
+netCfg = myGraphConfig
+  { graphDataColors = [yellow1, yellow2]
+  , graphLabel = Just "net "
+  }
 
-import System.Taffybar.FreedesktopNotifications
+memCfg = myGraphConfig
+  { graphDataColors = [taffyBlue]
+  , graphLabel = Just "mem "
+  }
 
-import Graphics.UI.Gtk
-import System.Information.Memory
-import System.Information.CPU
+cpuCfg = myGraphConfig
+  { graphDataColors = [green1, green2]
+  , graphLabel = Just "cpu "
+  }
 
-textWidgetNew :: String -> IO Widget
-textWidgetNew str = do
-    box <- hBoxNew False 0
-    label <- labelNew $ Just str
-    boxPackStart box label PackNatural 0
-    widgetShowAll box
-    return $ toWidget box
-
+memCallback :: IO [Double]
 memCallback = do
   mi <- parseMeminfo
   return [memoryUsedRatio mi]
 
 cpuCallback = do
-  (userLoad, systemLoad, totalLoad) <- cpuLoad
+  (_, systemLoad, totalLoad) <- cpuLoad
   return [totalLoad, systemLoad]
 
 main = do
-    let clock = textClockNew Nothing "<span fgcolor='#dfdfdf'>%-I:%M %p, %D</span>" 1
-        pager = taffyPagerNew defaultPagerConfig
-            { widgetSep = " :: "
-            , activeWorkspace = colorize "#ffaa00" "" . escape
-            , visibleWorkspace = colorize "#aa5500" "" . escape
-            --, emptyWorkspace = colorize "#444444" "" . escape
-            }
-        -- note = notifyAreaNew defaultNotificationConfig
-        tray = systrayNew
-        sep = textWidgetNew " ::"
-        memCfg = defaultGraphConfig { graphDataColors = [ (1, 0.1, 0.1, 1) ]
-                                    , graphBackgroundColor = (0.1, 0.1, 0.1)
-                                    , graphDirection = RIGHT_TO_LEFT
-                                    , graphPadding = 3
-                                    , graphWidth = 50
-                                    , graphBorderWidth = 0
-                                    , graphLabel = Just "mem"
-                                    }
-        cpuCfg = defaultGraphConfig {
-            graphDataColors = [ (1, 0.1, 0.1, 1) ]
-          , graphBackgroundColor = (0.1, 0.1, 0.1)
-          , graphDirection = RIGHT_TO_LEFT
-          , graphPadding = 3
-          , graphWidth = 50
-          , graphBorderWidth = 0
-          , graphLabel = Just "cpu"
+  let myWorkspacesConfig =
+        defaultWorkspacesConfig
+        { minIcons = 1
+        , widgetGap = 0
+        , showWorkspaceFn = hideEmpty
         }
-        mem = pollingGraphNew memCfg 1 memCallback
-        cpu = pollingGraphNew cpuCfg 1 cpuCallback
-        -- battery = batteryBarNew defaultBatteryConfig 10
-        -- batteryTime = textBatteryNew "| $time$" 10
-        --net = netMonitorNewWith 1k.5 "wlp3s0" 2 (formatNetworkInfo defaultNetFormat)
-        -- net = netMonitorNew 2 "wlp3s0"
-    defaultTaffybar defaultTaffybarConfig
-                        { startWidgets = [ pager ]
-                        , endWidgets = [ mem, cpu, clock, tray ]
-                        }
+      workspaces = workspacesNew myWorkspacesConfig
+      cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      mem = pollingGraphNew memCfg 1 memCallback
+      net = networkGraphNew netCfg Nothing
+      clock = textClockNew Nothing "<span fgcolor='#dfdfdf'>%-I:%M %p, %D</span>" 1
+      layout = layoutNew defaultLayoutConfig
+      windows = windowsNew defaultWindowsConfig
+      myConfig = defaultSimpleTaffyConfig
+        { startWidgets =
+            workspaces : map (>>= buildContentsBox) [ layout, windows ]
+        , endWidgets = map (>>= buildContentsBox)
+          [ batteryIconNew
+          , cpu
+          , mem
+          , net
+          , clock
+          , mpris2New
+          , sniTrayNew
+          ]
+        , barPosition = Top
+        , barPadding = 0
+        , barHeight = 35
+        , widgetSpacing = 0
+        }
+  dyreTaffybar $ withBatteryRefresh $ withLogServer $ withToggleServer $
+               toTaffyConfig myConfig
--- tag-gui/xmonad/xmonad.hs
+++ tag-gui/xmonad/xmonad.hs
@@ -16,11 +16,12 @@ import qualified XMonad.StackSet              as W
 import           XMonad.Util.EZConfig
 import           XMonad.Util.NamedScratchpad
 import           XMonad.Util.Run
+import           System.Taffybar.Support.PagerHints (pagerHints)
 
 
 main :: IO ()
 main = do
-  xmonad $ docks $ ewmh $ withUrgencyHook NoUrgencyHook $ myConfig
+  xmonad $ docks $ ewmh $ pagerHints $ myConfig
 
 myConfig = def
       { manageHook = myManageHook <+> manageHook def <+> manageDocks
@@ -36,8 +37,8 @@ myConfig = def
       } `additionalKeysP`
       [ ("M-p", spawn "rofi -show run -m -1 -font 'fantasque sans mono 12'")
       , ("M-S-p", spawn "rofi -show window -m -1 -font 'fantasque sans mono 12'")
-      , ("M-o", spawn "rofi -modi 'clipboard:~/.local/bin/greenclip print' -show clipboard -run-command '{cmd}'")
-      , ("M-S-l", spawn "~/.local/bin/better-slock")
+      , ("M-o", spawn "rofi -modi 'clipboard:greenclip print' -show clipboard -run-command '{cmd}'")
+      , ("M-S-l", spawn "slock")
       , ("M-b", sendMessage ToggleStruts)
       , ("M-s", namedScratchpadAction scratchpads "scratch")
       , ("M-u", namedScratchpadAction scratchpads "mail")
